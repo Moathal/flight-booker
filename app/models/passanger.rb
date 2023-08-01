e367a4508473666a8c7a1f6c79f259b135a22a13
Moathal <moathal.s.kachi@gmail.com>
2023-08-01 20:33:58 +0300

    Add devise

--- app/models/passanger.rb
+++ app/models/passanger.rb
@@ -1,4 +1,8 @@
-class passenger < ApplicationRecord
+class Passanger < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :flights, through: :tickets, source: :tickets_table_foreign_key_to_flights_table
-  has_many: tickets
+  has_many :tickets
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :passangers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "flights#index"
 end
