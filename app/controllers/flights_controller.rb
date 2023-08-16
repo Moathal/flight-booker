ea16c150003b1548350c44cae3b4b6c3afa9a98a
Moathal <moathal.s.kachi@gmail.com>
2023-08-16 20:59:55 +0300

    Fix seed and flight model

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,12 +3,12 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map { |airport| ["#{airport.name}, #{airport.code}, #{airport.city}, #{airport.country}", airport.id]}
     # parameters = search_params
-    @flights = search_params.nil? ? nil : Flight.where(search_params)
+      @flights = !params.nil? ? Flight.all.limit(8) : Flight.where(params[:departure_airport_id, :arrival_airport_id, :date])
   end
 
   private
 
-  def search_params
-    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :date)
-  end
+  # def search_params
+  #  params.nil? ? nil :  
+  # end
 end
