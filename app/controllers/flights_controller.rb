4f50eed7e3e03e3d0051ce947e11133a096678f8
Moathal <moathal.s.kachi@gmail.com>
2023-08-17 21:21:11 +0300

    Finish search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,12 +3,12 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map { |airport| ["#{airport.name}, #{airport.code}, #{airport.city}, #{airport.country}", airport.id]}
     # parameters = search_params
-      @flights = !params.nil? ? Flight.all.limit(8) : Flight.where(params[:departure_airport_id, :arrival_airport_id, :date])
+    @flights = params.nil? ? Flight.all.limit(8) : Flight.where(search_params)
   end
 
   private
 
-  # def search_params
-  #  params.nil? ? nil :  
-  # end
+  def search_params
+    params.permit(:departure_airport_id, :arrival_airport_id, :start_time)
+  end
 end
