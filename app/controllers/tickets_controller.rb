c3a6cdc3c28d68739b5aac5e5881c00465e4d827
Moathal <moathal.s.kachi@gmail.com>
2023-09-23 21:34:14 +0300

    Add turbo frames for search form

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,12 +2,7 @@ class TicketsController < ApplicationController
   before_action :authenticate_passenger!
 
   def index
-    puts "<><><><><><><><><><><><><><><>current_passenger_id: #{Ticket.where(booker_id: current_passenger.id).or(Ticket.where(passenger_id: current_passenger.id)).each do |single|
-      single.passenger_name
-    end
-    }<><><><><><><><><><><>"
     @tickets = Ticket.where(booker_id: current_passenger.id).or(Ticket.where(passenger_id: current_passenger.id))
-    @tickets.each {|ticket| "<><><><><><><><><><><><><><><><><><><><><> #{ticket.passenger_name} <><><><><><><><><><><><><><><><><><><>><><><><><><><><"}
   end
 
   def new
@@ -27,16 +22,12 @@ class TicketsController < ApplicationController
 
   def ticket
     passenger = Passenger.find_by(email: ticket_params[:passenger_email].downcase.strip)
-    puts "<><>}{}{><><><><}P}P}P}P}P}P}P}P} #{ticket_params[:passenger_email]} <><><><><><><><}{}{}{}{}{}{}{}{P{P{P{P{P}}}}}"
-    puts "<><>}{}{><><><><}P}P}P}P}P}P}P}P} #{Passenger.find_by(email: ticket_params[:passenger_email])} <><><><><><><><}{}{}{}{}{}{}{}{P{P{P{P{P}}}}}"
-    if passenger.nil?
     @ticket = Ticket.new(ticket_params)
+    if passenger.nil?
     @ticket.passenger = nil
     elsif passenger == current_passenger
-      @ticket = Ticket.new(ticket_params)
       @ticket.passenger = current_passenger
     else
-      @ticket = Ticket.new(ticket_params)
       @ticket.passenger = passenger
     end
     @ticket.booker = current_passenger
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,13 +4,30 @@ class Ticket < ApplicationRecord
   belongs_to :flight
 
   validate :unique_email_per_flight
+  validate :physical_capability
   before_save {self.passenger_email = passenger_email.downcase.strip}
 
   private
 
   def unique_email_per_flight
     if Ticket.exists?(flight_id: flight_id, passenger_email: passenger_email)
-      errors.add(:email, "This Passenger is already added to this flight")
+      errors.add(:passenger_email, "This Passenger is already added to this flight")
+    end
+  end
+  # I know that I can do it using validate-overlap gem but it was cool to solve it myself.
+  def physical_capability
+    tickets = Ticket.where(passenger_email: self.passenger_email)
+    this_flight_duration = Time.strptime(self.duration, "%H:%M") + 2.hours
+    this_flight_reserved_datetime = self.start_time.to_time - 1.hours + this_flight_duration.seconds_since_midnight
+    
+    tickets.each do |ticket|
+      other_flight_duration = Time.strptime(ticket.duration, "%H:%M") + 2.hours
+      other_flight_reserved_datetime = ticket.start_time.to_time - 1.hours + other_flight_duration.seconds_since_midnight
+
+      if self.start_time.to_time <= other_flight_reserved_datetime && self.start_time.to_time >= ticket.start_time.to_time - 1.hours ||
+        ticket.start_time.to_time <= this_flight_reserved_datetime && ticket.start_time.to_time >= self.start_time.to_time - 1.hours 
+        errors.add(:start_time, "You already have a flight in the specified time!!")
+      end
     end
   end
 end
